c646b04983f6174925857056154967eea63cd7a2
Kisahomi <magnemite72@gmail.com>
2025-06-27 14:08:49 +0800

    added more methods

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import {ENV} from "./config/env.js";
 import {db} from "./config/db.js";
 import { and, eq } from "drizzle-orm";
-import {sessions, sessionDay} from "./db/schema.js";
+import {sessions, sessionDay, sessionWorkout} from "./db/schema.js";
 import job from "./config/cron.js";
 
 const app = express();
@@ -21,7 +21,7 @@ app.get("/api/health", (req, res) => {
 app.post("/api/sessions", async (req, res) => {
 
     try {
-        const {name} = req.body;
+        const {name} = req.body; //same as saying  req.body.name
         
         if (!name){
             return res.status(400).json({error: "Missing required fields name from sessions"})
@@ -40,7 +40,7 @@ app.post("/api/sessions", async (req, res) => {
     }
 })
 
-app.post("/api/session-day", async (req, res) => {
+app.post("/api/session-days", async (req, res) => {
     try {
       const { sessionId, daysOfWeek } = req.body; //same dapat sessionId sa id ng sessions table
   
@@ -55,18 +55,70 @@ app.post("/api/session-day", async (req, res) => {
   
       res.status(201).json(newSessionDay[0]);
     } catch (error) {
-      console.log("Error adding session day", error);
+      console.log("Error adding session day/s", error);
       res.status(500).json({ error: "Something went wrong in adding session day" });
     }
 });
 
-app.get("/api/sessions/allname", async (req,res) => {
+app.post("/api/session-workouts", async (req, res) => {
+
+  try {
+    
+    const { sessionId, workoutName, sets } = req.body;
+
+    if (!sessionId || !workoutName || !sets) {
+      res.status(400).json({error: "Missing required fields: sessionId, workoutName, sets"})
+    }
+
+    const newSessionWorkout = await db.insert(sessionWorkout).values({
+      sessionId,
+      workoutName,
+      sets
+    }).returning();
+
+    res.status(201).json(newSessionWorkout[0])
+
+  } catch (error) {
+    console.log("Error adding session day", error);
+      res.status(500).json({ error: "Something went wrong in adding session workout" });
+  }
+})
+
+app.get("/api/session-workouts/allworkouts", async (req,res) => { //to get all session workouts and set
     
   try {
    
 
-    const sessionName = await db.select().from(sessions);
-    res.status(200).json(sessionName);
+    const getSessionWorkouts = await db.select().from(sessionWorkouts);
+    res.status(200).json(getSessionWorkouts);
+
+  } catch (error) {
+    console.log("Error fetching session name: ", error)
+    res.status(500).json({error: "Something went wrong with fetching"})
+  }
+})
+
+app.get("/api/session-days/alldays", async (req,res) => { //to get all session days
+    
+  try {
+   
+
+    const getSessionDays = await db.select().from(sessionDay);
+    res.status(200).json(getSessionDays);
+
+  } catch (error) {
+    console.log("Error fetching session name: ", error)
+    res.status(500).json({error: "Something went wrong with fetching"})
+  }
+})
+
+app.get("/api/sessions/allname", async (req,res) => { //to get all session name
+    
+  try {
+   
+
+    const getSessionName = await db.select().from(sessions);
+    res.status(200).json(getSessionName);
 
   } catch (error) {
     console.log("Error fetching session name: ", error)
